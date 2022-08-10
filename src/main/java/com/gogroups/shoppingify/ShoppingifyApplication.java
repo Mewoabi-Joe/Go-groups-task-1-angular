d739831019bce978793d15e3e0f34b6a7b0052d2
Lekeaka Joe <lekeakajoe@gmail.com>
2022-08-10 13:11:45 +0100

    big push, project complete

---
+++ src/main/java/com/gogroups/shoppingify/ShoppingifyApplication.java
@@ -0,0 +1,13 @@
+package com.gogroups.shoppingify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShoppingifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShoppingifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gogroups/shoppingify/ShoppingifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gogroups.shoppingify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
